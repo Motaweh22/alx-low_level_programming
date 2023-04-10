2d3821a3599137647016bd09b9810560b38d9bc0
Motaweh22 <m720.nagi2003@gmail.com>
2023-04-09 20:14:03 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * checker - checker
+ * @strr: array_string
+ *
+ * Return: Always 0 (Success)
+ */
+int checker(char *strr)
+{
+	unsigned int cc;
+
+	cc = 0;
+	while (cc < strlen(strr))
+
+	{
+		if (!isdigit(strr[cc]))
+		{
+			return (0);
+		}
+
+		cc++;
+	}
+	return (1);
+}
 
 /**
- * main - program that adds positive numbers.
- * @argc: counter
+ * main -  program that adds positive numbers.
+ * @argc: Counter
  * @argv: array of variable
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
-{
-	int addition = 0;
-	char *x;
 
-	while (--argc)
+int main(int argc, char *argv[])
+
+{
+
+	int ii;
+	int strint;
+	int addition = 0;
+
+	ii = 1;
+	while (ii < argc)
 	{
-		for (x = argv[argc]; *x; x++)
-			if (*x < '0' || *x > '9')
-				return (printf("Error\n"), 1);
-		addition += atoi(argv[argc]);
+		if (checker(argv[ii]))
+
+		{
+			strint = atoi(argv[ii]);
+			addition += strint;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		ii++;
 	}
-	printf("%d/n", addition);
+
+	printf("%d\n", addition);
 
 	return (0);
 }
