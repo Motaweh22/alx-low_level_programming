f2d542d1b830fee724156e5d063fc06c5dfbdfac
Motaweh22 <m720.nagi2003@gmail.com>
2023-04-09 19:22:10 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: counter
+ * @argv: array of variable
+ *
+ * Return: 0 if true and none zero if false
+ */
+int main(int argc, char *argv[])
+{
+	int m;
+
+	if (argc == 3)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+	}
+	else
+	{
+		printf("wrong inpust");
+		return (1);
+	}
+
+	return (0);
+}
